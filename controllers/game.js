bce573ce0f77faf35091cf72af83ffd9dd64f2ac
Kris Wong <krischwong@gmail.com>
2025-10-02 23:43:04 -0400

    prisma init

--- controllers/game.js
+++ controllers/game.js
@@ -1,5 +1,8 @@
 const picture = (req, res) => {
-  res.json("conencteed");
+  console.log(req.body.x);
+  console.log(req.body.y);
+  console.log(req.body.pick);
+  res.json({ x: req.body.x, y: req.body.y, pick: req.body.pick });
 };
 
 export default { picture };
