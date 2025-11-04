cb68a1056f2b776caf3a275da0c510df19823f5f
Kris Wong <krischwong@gmail.com>
2025-11-03 22:17:54 -0500

    Added check for correct guess and pushing obj to db

--- controllers/game.js
+++ controllers/game.js
@@ -1,19 +1,30 @@
 import db from "../db/queries.js";
 
 const picture = async (req, res) => {
-  const map = await db.getMap();
+  const mapPick = req.params.picture == "a" ? "kanagawa" : null;
+  const map = await db.getMap(mapPick);
+  const gameSessionID = req.body.id;
+  const gameSession = await db.gameSession(gameSessionID);
   const pick = req.body.pick;
-  const mapX = Math.round((req.body.x / req.body.width) * 100);
-  const mapY = Math.round((req.body.y / req.body.height) * 100);
-  const userX = map[`${pick}x`];
-  const userY = map[`${pick}y`];
-  const correctX = mapX - 3 <= userX && mapX + 3 >= userX;
-  const correctY = mapY - 3 <= userY && mapY + 3 >= userY;
-  const result = correctX && correctY;
-
-  console.log(result);
-
-  res.json({ pick, result });
+  if (!gameSession.guessedObj.includes(pick)) {
+    const mapX = Math.round((req.body.x / req.body.width) * 100);
+    const mapY = Math.round((req.body.y / req.body.height) * 100);
+    const userX = map[0][`${pick}x`];
+    const userY = map[0][`${pick}y`];
+    const correctX = mapX - 3 <= userX && mapX + 3 >= userX;
+    const correctY = mapY - 3 <= userY && mapY + 3 >= userY;
+    const result = correctX && correctY;
+    result ? await db.addPick(gameSessionID, pick) : null;
+    const gameOver = await db.gameSession(gameSessionID);
+    const status = gameOver.guessedObj.length < 3 ? false : true;
+    return res.json({ pick, result, status });
+  }
+  return res.send("Already guessed!");
 };
 
-export default { picture };
+const createPlayer = async (req, res) => {
+  const player = await db.createPlayer();
+  res.json({ id: player.id, createAt: player.createAt });
+};
+
+export default { picture, createPlayer };
