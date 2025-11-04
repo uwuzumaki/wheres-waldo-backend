2ffe1f9f55d6c8eee573366fc55c96b0f752eedf
Kris Wong <krischwong@gmail.com>
2025-11-03 23:05:31 -0500

    Create player route

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -4,5 +4,6 @@ import gameController from "../controllers/game.js";
 const router = Router();
 
 router.post("/:picture", gameController.picture);
+router.get("/newPlayer", gameController.createPlayer);
 
 export default router;
