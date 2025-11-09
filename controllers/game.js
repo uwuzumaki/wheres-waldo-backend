f0616a143577f42433b5db8e66066345c54a6c61
Kris Wong <krischwong@gmail.com>
2025-11-08 21:36:50 -0500

    updated createplayer and created gameover sequence

--- controllers/game.js
+++ controllers/game.js
@@ -1,7 +1,7 @@
 import db from "../db/queries.js";
 
 const picture = async (req, res) => {
-  const mapPick = req.params.picture == "a" ? "kanagawa" : null;
+  const mapPick = req.params.picture;
   const map = await db.getMap(mapPick);
   const gameSessionID = req.body.id;
   const gameSession = await db.gameSession(gameSessionID);
@@ -17,14 +17,30 @@ const picture = async (req, res) => {
     result ? await db.addPick(gameSessionID, pick) : null;
     const gameOver = await db.gameSession(gameSessionID);
     const status = gameOver.guessedObj.length < 3 ? false : true;
+    if (status) {
+      const finishTime = new Date();
+      const createAt = gameSession.createAt;
+      const totalTime = finishTime - createAt;
+      await db.finish(gameSessionID, finishTime, totalTime);
+    }
     return res.json({ pick, result, status });
   }
   return res.send("Already guessed!");
 };
 
 const createPlayer = async (req, res) => {
-  const player = await db.createPlayer();
+  const map = await db.getMap(req.body.map);
+  const player = await db.createPlayer(map[0].id);
   res.json({ id: player.id, createAt: player.createAt });
 };
 
-export default { picture, createPlayer };
+const gameOver = async (req, res) => {
+  const gameSession = await db.gameSession(req.body.id);
+  const map = gameSession.mapID;
+  const topTen = await db.getHighScore(map);
+  console.log("123");
+  console.log(topTen);
+  res.json(gameSession);
+};
+
+export default { picture, createPlayer, gameOver };
