4109feba56187e8f6aa34d263a72d3049d7eef04
Kris Wong <krischwong@gmail.com>
2025-11-08 21:35:59 -0500

    updated routes

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -3,7 +3,8 @@ import gameController from "../controllers/game.js";
 
 const router = Router();
 
-router.post("/:picture", gameController.picture);
-router.get("/newPlayer", gameController.createPlayer);
+router.post("/map/:picture", gameController.picture);
+router.post("/newPlayer", gameController.createPlayer);
+router.post("/gameOver", gameController.gameOver);
 
 export default router;
