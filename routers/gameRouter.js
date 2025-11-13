9fd90f7677e64a416c86398d2558382c49a3a06e
Kris Wong <krischwong@gmail.com>
2025-11-12 23:28:17 -0500

    added route for current player

--- routers/gameRouter.js
+++ routers/gameRouter.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.post("/map/:picture", gameController.picture);
 router.post("/newPlayer", gameController.createPlayer);
+router.post("/currentPlayer", gameController.currentPlayer);
 router.post("/gameOver", gameController.gameOver);
 
 export default router;
