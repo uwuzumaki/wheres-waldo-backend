17a02eb17b3d90ea088a3a1d2c54f25e9b57ba4d
Kris Wong <krischwong@gmail.com>
2026-01-05 01:20:31 -0500

    testing to see if it's the url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://wheres-waldo2.netlify.app"],
+    origin: "*",
     credentials: true,
   })
 );
